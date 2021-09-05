261bc8ebbe30e4384a3454f010649a60cc9eca30
Simranjeet Singh <38141273+simranjeet97@users.noreply.github.com>
2021-09-05 20:53:43 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -380,6 +380,10 @@ app.layout = dbc.Container(fluid=True, children=[
     children=[
         html.Div(
             children=[
+                html.P(children="🦅", className="header-emoji"),
+                html.H1(
+                    children="FreeBirds Crew", className="header-title"
+                ),
                 html.P(
                     children="EDA on the Basis of Condition and Impact on Price",
                     className="header-description",
@@ -439,6 +443,10 @@ app.layout = dbc.Container(fluid=True, children=[
     children=[
         html.Div(
             children=[
+                html.P(children="🦅", className="header-emoji"),
+                html.H1(
+                    children="FreeBirds Crew", className="header-title"
+                ),
                 html.P(
                     children="Analysis on the basis of Transmission and Body and Which is Best !",
                     className="header-description",
